ca56505ecf51c87618b472de6488b90aeef15273
Youngmin Joung <ymjoung@amazon.com>
2024-07-29 22:19:27 +0900

    update main.tf

--- main.tf
+++ main.tf
@@ -92,8 +92,32 @@ module "lb_role" {
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"] # namespace:serviceaccount
     }
   }
+}
 
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
 
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.lb_role.iam_role_arn
+  }
 }
 
 
