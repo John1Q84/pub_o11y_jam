61c77d7ddc61190542beb4dbb4c89d6f66e588e5
Youngmin Joung <ymjoung@amazon.com>
2024-07-29 15:13:29 +0900

    update kubecluster 1.30

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   name            = "eks-jam"
   region          = data.aws_region.current.name
-  cluster_version = "1.29"
+  cluster_version = "1.30"
   vpc_cidr        = "10.0.0.0/16"
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
 
--- script/runnable_userdata.sh
+++ script/runnable_userdata.sh
@@ -78,9 +78,11 @@ install_tools(){
     sudo ./aws/install
     rm awscliv2.zip
 
-    # Install kubectl v1.24
+    # Install kubectl v1.30
     #curl -o /tmp/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-01-30/bin/linux/amd64/kubectl
-    curl -o /tmp/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+    #curl -o /tmp/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+    curl -o /tmp/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+
 
     sudo mv /tmp/kubectl /usr/local/bin
     chmod +x /usr/local/bin/kubectl
