5bdb2ea7ab4fcff4c3df9fccd293e9bbb246ae6e
Youngmin Joung <ymjoung@amazon.com>
2024-07-29 20:12:39 +0900

    update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   #version = "~> 3.0"
-  version = "5.1.1"
+  version = "5.9.0"
 
   name = local.name ## 모든 resource의 Name tag에 추가 됩니다.
   cidr = local.vpc_cidr
@@ -41,69 +41,115 @@ module "vpc" {
 
 }
 
-module "eks_blueprints" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1" # module version pinned. EKS blue print v5 does not provide backward comparibility
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-  cluster_name              = local.name
-  vpc_id                    = module.vpc.vpc_id
-  private_subnet_ids        = module.vpc.private_subnets
-  cluster_version           = local.cluster_version
-  cluster_enabled_log_types = []
-  enable_cluster_encryption = false
+  cluster_name                   = local.name
+  cluster_version                = local.cluster_version
+  cluster_endpoint_public_access = true
 
-  map_roles = [
-    {
-      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TeamRole"
-      username = "ops-role"         # The user name within Kubernetes to map to the IAM role
-      groups   = ["system:masters"] # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
-    }
-  ]
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
 
-  # EKS MANAGED NODE GROUPS
-  managed_node_groups = {
-    mg_5 = {
-      node_group_name = local.node_group_name
-      instance_types  = ["m5.large"]
-      subnet_ids      = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    instance_types = ["m5.large"]
+  }
+
+  eks_managed_node_groups = {
+    sample = {
+      instance_types = ["m5.large"]
+      min_size       = 2
+      max_size       = 5
+      desired_size   = 2
     }
   }
 
-  tags = local.tags
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    ops-role = {
+      kubernetes_groups = ["system:masters"]
+      principal_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TeamRole"
+      type              = "ARN"
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "eks-jam"
+  }
 }
+
+#module "eks_blueprints" {
+#  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1" # module version pinned. EKS blue print v5 does not provide backward comparibility
+#
+#  cluster_name              = local.name
+#  vpc_id                    = module.vpc.vpc_id
+#  private_subnet_ids        = module.vpc.private_subnets
+#  cluster_version           = local.cluster_version
+#  cluster_enabled_log_types = []
+#  enable_cluster_encryption = false
+#
+#  map_roles = [
+#    {
+#      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TeamRole"
+#      username = "ops-role"         # The user name within Kubernetes to map to the IAM role
+#      groups   = ["system:masters"] # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
+#    }
+#  ]
+#
+#  # EKS MANAGED NODE GROUPS
+#  managed_node_groups = {
+#    mg_5 = {
+#      node_group_name = local.node_group_name
+#      instance_types  = ["m5.large"]
+#      subnet_ids      = module.vpc.private_subnets
+#    }
+#  }
+#
+#  tags = local.tags
+#}
 
 #   ## EKS add-ons
 
 
-module "kubernetes_addons" {
-  #source         = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.27.0/modules/kubernetes-addons"
-  source         = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1/modules/kubernetes-addons"
-  eks_cluster_id = module.eks_blueprints.eks_cluster_id
-
-  ## EKS Addons
-  enable_aws_load_balancer_controller = true
-  aws_load_balancer_controller_helm_config = {
-    name            = "aws-load-balancer-controller"
-    chart           = "aws-load-balancer-controller"
-    service_account = "aws-lb-sa"
-    namespace       = "kube-system"
-  }
-
-  enable_amazon_eks_vpc_cni = true
-  amazon_eks_vpc_cni_config = {
-    addon_name      = "vpc-cni"
-    addon_version   = local.eks_addon_vpc_cni_version
-    service_account = "aws-node"
-  }
-  enable_amazon_eks_coredns = true
-  amazon_eks_coredns_config = {
-    addon_name      = "coredns"
-    addon_version   = local.eks_addon_coredns_version
-    service_account = "coredns"
-  }
-  enable_amazon_eks_kube_proxy = true
-  amazon_eks_kube_proxy_config = {
-    addon_name      = "kube-proxy"
-    addon_version   = local.eks_addon_kube_proxy_version
-    service_account = "kube-proxy"
-  }
-}
+#module "kubernetes_addons" {
+#  #source         = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.27.0/modules/kubernetes-addons"
+#  source         = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1/modules/kubernetes-addons"
+#  eks_cluster_id = module.eks_blueprints.eks_cluster_id
+#
+#  ## EKS Addons
+#  enable_aws_load_balancer_controller = true
+#  aws_load_balancer_controller_helm_config = {
+#    name            = "aws-load-balancer-controller"
+#    chart           = "aws-load-balancer-controller"
+#    service_account = "aws-lb-sa"
+#    namespace       = "kube-system"
+#  }
+#
+#  enable_amazon_eks_vpc_cni = true
+#  amazon_eks_vpc_cni_config = {
+#    addon_name      = "vpc-cni"
+#    addon_version   = local.eks_addon_vpc_cni_version
+#    service_account = "aws-node"
+#  }
+#  enable_amazon_eks_coredns = true
+#  amazon_eks_coredns_config = {
+#    addon_name      = "coredns"
+#    addon_version   = local.eks_addon_coredns_version
+#    service_account = "coredns"
+#  }
+#  enable_amazon_eks_kube_proxy = true
+#  amazon_eks_kube_proxy_config = {
+#    addon_name      = "kube-proxy"
+#    addon_version   = local.eks_addon_kube_proxy_version
+#    service_account = "kube-proxy"
+#  }
+#}
