ca137435d35cbc280e206126434842162b6382d3
Youngmin Joung <ymjoung@amazon.com>
2024-08-02 18:24:12 +0900

    update terraform code

--- terraform/alb/data.tf
+++ terraform/alb/data.tf
@@ -3,6 +3,5 @@ data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster_auth" "this" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
+  name = data.terraform_remote_state.eks.outputs.eks_cluster_name
 }
-
--- terraform/alb/provider.tf
+++ terraform/alb/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.9.0" # 원하는 버전을 지정하세요
+      version = ">= 2.9.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.10.0" # Kubernetes 프로바이더도 필요할 수 있습니다
+      version = "~> 2.10.0"
     }
     aws = {
       source  = "hashicorp/aws"
@@ -21,29 +21,18 @@ provider "aws" {
 }
 
 
-#provider "helm" {
-#  kubernetes {
-#    host                   = data.aws_eks_cluster.cluster.endpoint
-#    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority)
-#    exec {
-#      api_version = "client.authentication.k8s.io/v1beta1"
-#      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-#      command     = "aws"
-#    }
-#  }
-#}
 
 provider "helm" {
   kubernetes {
-    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+    host                   = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_ca_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
 
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority)
+  host                   = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_cluster_ca_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
