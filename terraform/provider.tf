43623434a4b2092027cb60388b7a2b26ab339441
Youngmin Joung <ymjoung@amazon.com>
2024-08-02 14:32:15 +0900

    rollback provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,26 +2,13 @@ terraform {
   required_version = ">= 1.3.8"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.30.0"
+      source = "hashicorp/aws"
+      #version = "~> 4.57.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
-
   }
 }
 
@@ -37,24 +24,23 @@ provider "aws" {
 #}
 
 provider "kubernetes" {
-  #host                   = module.eks.cluster_endpoint
-  host                   = data.eks.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.eks.cluster.ca_data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode((module.eks.cluster_certificate_authority_data))
   token                  = data.aws_eks_cluster_auth.this.token
 
 }
 provider "helm" {
   kubernetes {
-    host                   = data.eks.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.eks.cluster.ca_data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
 
 provider "kubectl" {
   apply_retry_count      = 10
-  host                   = data.eks.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.eks.cluster.ca_data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file       = false
   token                  = data.aws_eks_cluster_auth.this.token
 }
