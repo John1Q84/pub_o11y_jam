09feabcca92f13ec1b9581dcab37e47d394364d9
Youngmin Joung <ymjoung@amazon.com>
2024-08-02 14:51:17 +0900

    update alb

--- terraform/alb/provider.tf
+++ terraform/alb/provider.tf
@@ -1,1 +1,21 @@
-../provider.tf
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.5.0" # 원하는 버전을 지정하세요
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.10.0" # Kubernetes 프로바이더도 필요할 수 있습니다
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
--- terraform/alb/remote.tf
+++ terraform/alb/remote.tf
@@ -7,9 +7,12 @@ data "terraform_remote_state" "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.eks_cluster_name
+  name                  = data.terraform_remote_state.eks.outputs.eks_cluster_name
+  endpoint              = data.terraform_remote_state.eks.outputs.eks_cluster_endpoint
+  certificate_authority = data.terraform_remote_state.eks.outputs.eks_cluster_ca_data
 }
 
 data "lb_role" "arn" {
   arn = data.terraform_remote_state.eks.outputs.lb_role_arn
 }
+
